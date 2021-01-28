b4be7745a357b766f6a0ffdc8274fdcaac2375ab
Muhammad Gamal <56555715+MuhmdJimy@users.noreply.github.com>
2021-01-28 12:59:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/LoginPage.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(Signup_Signin_UI());
 }
 
